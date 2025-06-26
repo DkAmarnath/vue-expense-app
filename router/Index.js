16e34f40808c87f30966c93381638a408a296573
DkAmarnath <34921125+DkAmarnath@users.noreply.github.com>
2025-06-27 00:40:01 +0530

    Update Index.js

--- router/Index.js
+++ router/Index.js
@@ -1,15 +1,14 @@
+// router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
-import App from '../App.vue'
+import ExpenseTracker from '../App.vue'
 import MonthlyBudget from '../MonthlyBudget.vue'
 
 const routes = [
-  { path: '/', name: 'ExpenseTracker', component: App },
+  { path: '/', name: 'ExpenseTracker', component: ExpenseTracker },
   { path: '/monthly-budget', name: 'MonthlyBudget', component: MonthlyBudget }
 ]
 
-const router = createRouter({
+export default createRouter({
   history: createWebHistory(),
   routes
 })
-
-export default router
