56ed4af4a85a06286632628ce62aa30735a7c5d0
DkAmarnath <34921125+DkAmarnath@users.noreply.github.com>
2025-06-27 00:42:58 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // main.js
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router/index.js'  // <-- MAKE SURE this path exists
+import router from '/router/index.js'  // <-- MAKE SURE this path exists
 
 createApp(App)
   .use(router)
