00b5681a6527406d8595971b880f2034fa78141b
Amarnath DK <dka914@gmail.com>
2025-07-08 21:47:01 +0530

    Updated11

--- App.vue
+++ App.vue
@@ -2,7 +2,7 @@
   <div>
     <nav style="background:#4caf50;padding:1em;">
       <router-link to="/" style="margin-right:1em;color:white;">Home</router-link>
-      <router-link to="/budget" style="color:white;">Budget</router-link>
+      <router-link to="/budget" style="margin-right:1em;color:white;">Budget</router-link>
       <router-link to="/tasks" style="color:white;">Tasks</router-link> 
     </nav>
     <router-view/>
